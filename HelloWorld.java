226b55c50b91ac2b997182d04a07c849865470fc
Naresh Muthavarapu <naresh.muthavarapu@gmail.com>
2017-12-05 14:55:20 +0530

    Helloworld modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("This is Dilip from Hyderabad");
+    System.out.println("This is Dilip from Chennai");
   }
 }
